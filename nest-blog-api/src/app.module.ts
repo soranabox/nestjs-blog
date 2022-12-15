54eef492cce178213d94ee44be97e8ed0208de18
soranabox <soranabox@gmail.com>
2022-12-15 14:30:38 +0700

    config DB

--- nest-blog-api/src/app.module.ts
+++ nest-blog-api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './core/database/database.module';
+import { ConfigModule } from '@nestjs/config/dist';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
